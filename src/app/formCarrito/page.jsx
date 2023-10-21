7fa7c321dd0566b92ef40157ddbd3a557a0e468f
ImanolBraccciale <imanol11699@outlook.com>
2023-10-20 22:33:33 -0300

    borre registro de ventras, ventas  e index.js que esta en page

--- src/app/formCarrito/page.jsx
+++ src/app/formCarrito/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-import { useRouter } from "next/router";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import style from "./page.module.css";
@@ -9,12 +8,7 @@ import BackButtom from "../componentes/BackButtom/BackButtom";
 import ProductListCart from "../componentes/ProductListCart/ProductListCart";
 import ProductBarCart from "../componentes/ProductBarCart/ProductBarCart";
 import { postSale } from "../redux/actions/actions";
-import { getUserByEmail } from "../redux/actions/actions";
-import s from "./page.module.css";
-import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
 import axios from "axios";
-
-initMercadoPago("TEST-2704ea0d-a660-4fe8-be61-e2c32675c7ed");
 
 function FormCarrito() {
   const dispatch = useDispatch();
@@ -33,14 +27,13 @@ function FormCarrito() {
   }, [user]);
 
   const productSummary = [];
-  const [preferenceId, setPreferenceId] = useState(null);
   const userActual = JSON.parse(user);
 
   allCartItems.forEach((item) => {
     const productName = item.Name;
     const quantity = item.Qty;
     const price = parseFloat(item.Price);
-    // Buscar si el producto ya existe en el arreglo productSummary
+ 
     const existingProduct = productSummary.find(
       (summaryItem) => summaryItem.Name === productName
     );
@@ -62,69 +55,33 @@ function FormCarrito() {
         "Debe agregar al menos un producto al carrito para poder generar la compra"
       );
     } else {
-      if (paymentMethod === "mercadoPago") {
-        const dataToSend = {
-          productSummary,
-          paymentMethod,
-        };
-        dispatch(postSale(dataToSend)).then(async (data) => {
-          if (typeof data.payload === "object") {
-            console.log("Se registró la venta");
-            console.log("Pagar con mercado pago");
-            const subtotalsSum = productSummary.reduce(
-              (total, item) => total + item.Subtotal,
-              0
-            );
-            try {
-              console.log("create");
-              const response = await axios.post("/api/mercadoPago", {
-                product: {
-                  description: "Venta general",
-                  price: subtotalsSum,
-                  quantity: 1,
-                  currency_id: "ARS",
-                },
-              });
-
-              const { id } = response.data;
-              console.log("sample");
-              if (id) {
-                setPreferenceId(id);
-              }
-            } catch (error) {
-              console.error("Error al crear la preferencia:", error);
-            }
-          } else {
-            alert(
-              "Hubo un error al generar su compra, por favor inténtelo nuevamente."
+      const dataToSend = {
+        productSummary,
+        paymentMethod,
+      };
+      dispatch(postSale(dataToSend)).then((data) => {
+        if (typeof data.payload === "object") {
+          try {
+            axios.post("/api/nodemailer", {
+              subject: "Confirmación de compra",
+              toEmail: userActual.emailUser,
+              productSummary,
+              paymentMethod,
+              isPurchase: true,
+            });
+            alert("Su compra se realizó exitósamente.");
+            window.location.href = "/";
+          } catch (error) {
+            console.error(
+              "No se pudo obtener el correo electrónico del usuario."
             );
           }
-        });
-      } else {
-        const dataToSend = {
-          productSummary,
-          paymentMethod,
-        };
-        dispatch(postSale(dataToSend)).then((data) => {
-          if (typeof data.payload === "object") {
-            try {
-              axios.post("/api/nodemailer", {
-                subject: "Confirmación de compra",
-                toEmail: userActual.emailUser,
-                productSummary,
-                paymentMethod,
-                isPurchase: true,
-              });
-              alert("Su compra se realizó exitósamente.");
-              window.location.href = "/";
-            } catch (error) {
-              console.error(
-                "No se pudo obtener el correo electrónico del usuario."
-              );
-            }
-          }
-        });
-      }
+        } else {
+          alert(
+            "Hubo un error al generar su compra, por favor inténtelo nuevamente."
+          );
+        }
+      });
     }
   };
 
@@ -154,13 +111,8 @@ function FormCarrito() {
       >
         <option value="efectivo">Efectivo</option>
         <option value="tarjeta">Tarjeta</option>
-        <option value="mercadoPago">MercadoPago</option>
+
       </select>
-      {preferenceId && (
-        <div>
-          <Wallet initialization={{ preferenceId }} className={s.wallet} />
-        </div>
-      )}
       <Link href="/">
         <BackButtom />
       </Link>
